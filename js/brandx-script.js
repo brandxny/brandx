a7fc920f7ae967c07b87f2ea8b211deb6ae3b7b2
juanmriosv <juanma@72-ec-jr3.myfiosgateway.com>
2021-12-29 18:03:12 -0500

    cleaning scriptds

--- js/brandx-script.js
+++ js/brandx-script.js
@@ -204,16 +204,3 @@ if(screenWidth>screenThreshold)
 }};
 
 deviceRouter();
-
-const openMobileMenuBtn = document.querySelector(".bx-device-menu");
-const headerMenu = document.querySelector(".bx-header-nav");
-
-openMobileMenuBtn.addEventListener("click", () => {
-  if(openMobileMenuBtn.classList.contains("open")) {
-    openMobileMenuBtn.classList.remove("open");
-    headerMenu.classList.remove("active");
-  } else {
-    headerMenu.classList.add("active");
-    openMobileMenuBtn.classList.add("open");
-  }
-})
